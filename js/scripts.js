aa6e34b83878fde015481fafa8d3a1bcdd1639f2
Kennedy-karuri <kennkaruri99@gmail.com>
2020-10-12 08:33:24 +0300

    modified js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -36,77 +36,76 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $("#jim1").mouseover(function () {
-            $("#jim2").show();
-        })
+        $("#jim2").show();
+    })
         .mouseout(function () {
             $("#jim2").hide();
         });
 
-        $("#yellow1").mouseover(function(){
-            $("#yellow2").show();
-        })
-        .mouseout(function(){
+    $("#yellow1").mouseover(function () {
+        $("#yellow2").show();
+    })
+        .mouseout(function () {
             $("#yellow2").hide()
         });
 
-        $("#mighty1").mouseover(function(){
-            $("#mighty2").show();
-        })
-        .mouseout(function(){
+    $("#mighty1").mouseover(function () {
+        $("#mighty2").show();
+    })
+        .mouseout(function () {
             $("#mighty2").hide()
         });
 
-        $("#dark1").mouseover(function(){
-            $("#dark2").show();
-        })
-        .mouseout(function(){
+    $("#dark1").mouseover(function () {
+        $("#dark2").show();
+    })
+        .mouseout(function () {
             $("#dark2").hide();
         })
 
-        $("#have1").mouseover(function(){
-            $("#have2").show();
-        })
-        .mouseout(function(){
+    $("#have1").mouseover(function () {
+        $("#have2").show();
+    })
+        .mouseout(function () {
             $("#have2").hide();
         })
 
-        $("#selector1").mouseover(function(){
-            $("#selector2").show();
+    $("#selector1").mouseover(function () {
+        $("#selector2").show();
+    })
+        .mouseout(function () {
+            $("#selector2").hide();
         })
-         .mouseout(function(){
-             $("#selector2").hide();
-         })
 
-         $("#burned1").mouseover(function(){
-             $("#burned2").show();
-         })
-         .mouseout(function(){
-             $("#burned2").hide();
-         })
+    $("#burned1").mouseover(function () {
+        $("#burned2").show();
+    })
+        .mouseout(function () {
+            $("#burned2").hide();
+        })
 
-         $("#giraffe1").mouseover(function(){
-             $("#giraffe2").show();
-         })
-         .mouseout(function(){
-             $("#giraffe2").hide();
-         })
-         });
+    $("#giraffe1").mouseover(function () {
+        $("#giraffe2").show();
+    })
+        .mouseout(function () {
+            $("#giraffe2").hide();
+        })
+});
 
-        
-        $("button").click(function () {
-            var name = document.getElementById("ken1").value;
-            var email = document.getElementById("mce-EMAIL").value;
-            var message = document.getElementById("ken2").value;
-            if (name === "") {
-              alert("Please Insert your name")
-            } else
-                 if(email === ""){
-                alert("Enter your Email")
-            }else
-               if(message === "") {
-              alert("Please insert your message!")
-            }else {
-               alert(' Dear ' + name + ' we have received your message successfully. ' + ' Feel free to reach out to us at anytime');
+
+$("button").click(function () {
+    var name = document.getElementById("ken1").value;
+    var email = document.getElementById("ken2").value;
+    var message = document.getElementById("ken3").value;
+    if (name === "") {
+        alert("Please Insert your name")
+    } else
+        if (email === "") {
+            alert("Enter your Email")
+        } else
+            if (message === "") {
+                alert("Please insert your message!")
+            } else {
+                alert(' Dear ' + name + ' we have received your message successfully. ' + ' Feel free to reach out to us at anytime');
             }
-          });
-            
+});
